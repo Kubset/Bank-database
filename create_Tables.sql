99d6a94d3ee7ffe781977190d855a68ecf8621ca
Kubset <kuba1228334@gmail.com>
2018-09-17 14:44:16 +0200

    fix bugs with references

--- create_Tables.sql
+++ create_Tables.sql
@@ -1,69 +1,79 @@
-CREATE TABLE account (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES user.id,
-  balance BIGING NOT NULL,
-  avalible BIGINT NOT NULL,
-  type_id INTEGER REFERENCES account_type.id
-);
-CREATE TABLE credit (
-  id SERIAL PRIMARY KEY,
-  account_id INTEGER REFERENCES account.id,
-  balance BIGINT NOT NULL,
-  principle BIGINT NOT NULL,
-  date DATE NOT NULL,
-  rate FLOAT NOT NULL
-);
-
-CREATE TABLE deposit (
-  id SERIAL PRIMARY KEY,
-  account_id INTEGER REFERENCES account.id,
-  balance BIGINT NOT NULL,
-  principle BIGINT NOT NULL,
-  date DATE NOT NULL,
-  rate FLOAT NOT NULL
-);
-
-CREATE TABLE card (
-  id SERIAL PRIMARY KEY,
-  account_id INTEGER REFERENCES account.id,
-  valid_from DATE NOT NULL,
-  expire DATE NOT NULL,
-  type_id INTEGER REFERENCES card_type.id
-);
-
-CREATE TABLE card_type (
-  id SERIAL PRIMARY KEY,
-  card_id INTEGER REFERENCES card.id
-);
-
-CREATE TABLE account_type (
-  id SERIAL PRIMARY KEY,
-  card_id INTEGER REFERENCES account.id
-);
-
-CREATE TABLE transaction (
-  id SERIAL PRIMARY KEY,
-  source_account_id INTEGER REFERENCES account.id,
-  target_account_id INTEGER REFERENCES account.id,
-  value BIGINT,
-  date TIMESTAMP
-);
-
-CREATE TABLE user_account (
-  user_id SERIAL PRIMARY KEY,
-  account_id INTEGER REFERENCES account.ud
-);
-
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   login TEXT,
   password TEXT
 );
 
-CREATE TABLE user_data (
-  user_id INTEGER REFERENCES user.id,
+
+CREATE TABLE IF NOT EXISTS user_data (
+  user_id INTEGER REFERENCES users,
   first_name TEXT,
   last_name TEXT,
   address TEXT,
   phone_number TEXT
 );
+
+CREATE TABLE IF NOT EXISTS  card_types (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS  account_types (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users,
+  balance BIGINT NOT NULL,
+  avalible BIGINT NOT NULL,
+  type_id INTEGER REFERENCES account_types
+);
+
+CREATE TABLE IF NOT EXISTS  cards (
+id SERIAL PRIMARY KEY,
+account_id INTEGER REFERENCES accounts,
+valid_from DATE NOT NULL,
+expire DATE NOT NULL,
+type_id INTEGER REFERENCES card_types
+);
+
+
+
+
+
+CREATE TABLE IF NOT EXISTS  user_accounts (
+  user_id SERIAL PRIMARY KEY,
+  account_id INTEGER REFERENCES accounts
+);
+
+CREATE TABLE IF NOT EXISTS  credits (
+  id SERIAL PRIMARY KEY,
+  account_id INTEGER REFERENCES accounts,
+  balance BIGINT NOT NULL,
+  principle BIGINT NOT NULL,
+  date DATE NOT NULL,
+  rate FLOAT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS  deposits (
+  id SERIAL PRIMARY KEY,
+  account_id INTEGER REFERENCES accounts,
+  balance BIGINT NOT NULL,
+  principle BIGINT NOT NULL,
+  date DATE NOT NULL,
+  rate FLOAT NOT NULL
+);
+
+
+
+CREATE TABLE IF NOT EXISTS  transactions (
+  id SERIAL PRIMARY KEY,
+  source_account_id INTEGER REFERENCES accounts,
+  target_account_id INTEGER REFERENCES accounts,
+  value BIGINT,
+  date TIMESTAMP
+);
+
+
