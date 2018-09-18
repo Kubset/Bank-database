bae5b54468e6c796cc3b7bec084c5243498e59d3
Bart <bartpetka@gmail.com>
2018-09-18 13:13:35 +0200

    Add internal bank account

--- fill_account_types.sql
+++ fill_account_types.sql
@@ -1,3 +1,4 @@
 INSERT INTO account_types(id, name) VALUES (1, '500+');
 INSERT INTO account_types(id, name) VALUES (2, 'Regular Account');
 INSERT INTO account_types(id, name) VALUES (3, 'Platinum Account');
+INSERT INTO account_types(id, name) VALUES  (0, 'Internal transaction');
