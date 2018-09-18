6aab83e3822e387ada14147c0bbc7453337776a1
Bart <bartpetka@gmail.com>
2018-09-18 15:44:00 +0200

    Update trigger with ballance chcecking function

--- triggers.sql
+++ triggers.sql
@@ -3,4 +3,9 @@ AFTER INSERT ON transactions
 FOR EACH ROW
 EXECUTE PROCEDURE update_balances();
 
+CREATE TRIGGER ensure_ballance BEFORE INSERT
+  ON transactions
+  FOR EACH ROW
+  EXECUTE PROCEDURE check_ballance();
+
 
