fb2a7047ea1162cf5e026f997c8a3887b8ba4988
Kubset <kuba1228334@gmail.com>
2018-09-18 14:50:21 +0200

    add getbalance and updatebalance functions

---
+++ functions/getbalance@(integer).sql
@@ -0,0 +1,42 @@
+create function getbalance(userid integer)
+  returns integer
+language plpgsql
+as $$
+DECLARE
+  revenues BIGINT := 0;
+  expenditures BIGINT := 0;
+  count_transactions INTEGER := 0;
+
+BEGIN
+    count_transactions = (SELECT COUNT(*) FROM transactions
+                WHERE public.transactions.source_account_id = userid
+                OR transactions.target_account_id = userid);
+
+    revenues = (SELECT sum(value) FROM transactions
+                WHERE public.transactions.source_account_id = userid);
+    expenditures = (SELECT sum(value) FROM transactions
+              WHERE public.transactions.target_account_id = userid);
+
+--   raise notice 'Valuecount: %', count_transactions;
+--   raise notice 'Valueexp: %', expenditures;
+--   raise notice 'Valuerev: %', revenues;
+
+
+  IF count_transactions = 0 THEN
+    RETURN 0;
+  end if;
+
+  IF revenues IS NOT NULL AND expenditures IS NOT NULL THEN
+    RETURN expenditures-revenues;
+  ELSEIF expenditures IS NULL THEN
+    RETURN -revenues;
+  elseif revenues IS NULL THEN
+    RETURN expenditures;
+  END IF;
+
+END;
+$$;
+
+alter function getbalance(integer)
+  owner to postgres;
+
---
+++ functions/update_balances@().sql
@@ -0,0 +1,16 @@
+create function update_balances()
+  returns trigger
+language plpgsql
+as $$
+BEGIN
+        UPDATE accounts
+        SET balance = getbalance(id)
+          WHERE id = NEW.target_account_id
+          OR id = NEW.source_account_id;
+  RETURN NEW;
+END;
+$$;
+
+alter function update_balances()
+  owner to postgres;
+
