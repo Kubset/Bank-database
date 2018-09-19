789819e35e97691da0b76565bca77ccafd4de884
Bart <bartpetka@gmail.com>
2018-09-19 11:34:01 +0200

    Add trigger for transactions

--- functions/triggers.sql
+++ functions/triggers.sql
@@ -3,4 +3,8 @@ AFTER INSERT ON transactions
 FOR EACH ROW
 EXECUTE PROCEDURE update_balances();
 
+CREATE TRIGGER ensure_balance_before_transaction BEFORE INSERT
+  ON transactions
+  FOR EACH ROW
+  EXECUTE PROCEDURE check_balance_before_transaction();
 
