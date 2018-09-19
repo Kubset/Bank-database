1365088a1de3b601dee5a3980469677640d25d28
Bart <bartpetka@gmail.com>
2018-09-19 11:37:31 +0200

    Implement function for adding sum of credits into available founds in account (random data proceed)

---
+++ functions/update_available_founds_with_credits@().sql
@@ -0,0 +1,22 @@
+create function update_available_founds_with_credits()
+  returns void
+language plpgsql
+as $$
+DECLARE
+   t_row accounts%rowtype;
+    pre_available INTEGER;
+
+BEGIN
+    FOR t_row in SELECT * FROM accounts LOOP
+      pre_available = t_row.available;
+        update accounts
+            set available = (pre_available + sum_of_credits(t_row.id))
+        where id = t_row.id;
+    END LOOP;
+  RETURN;
+END;
+$$;
+
+alter function update_available_founds_with_credits()
+  owner to bartoszjakub;
+
