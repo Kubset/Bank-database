d3120c59b5bc03deaa1e161d5796595977e94bd4
Kubset <kuba1228334@gmail.com>
2018-09-20 12:52:29 +0200

    add new trigger to create account to user

--- functions/triggers.sql
+++ functions/triggers.sql
@@ -1,10 +1,15 @@
 CREATE TRIGGER recalculate
-AFTER INSERT ON transactions
-FOR EACH ROW
-EXECUTE PROCEDURE update_balances();
+  AFTER INSERT ON transactions
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_balances();
 
 CREATE TRIGGER ensure_balance_before_transaction BEFORE INSERT
   ON transactions
   FOR EACH ROW
   EXECUTE PROCEDURE check_balance_before_transaction();
+
+CREATE TRIGGER create_account_when_create_user
+  AFTER INSERT ON users
+  FOR EACH ROW
+  EXECUTE PROCEDURE create_account();
 
