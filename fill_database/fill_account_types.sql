137e3659364acb76c68292fbdb1ccea9a977bbc3
Bart <bartpetka@gmail.com>
2018-09-20 12:54:26 +0200

    Update account_types

--- fill_database/fill_account_types.sql
+++ fill_database/fill_account_types.sql
@@ -1,4 +1,6 @@
+INSERT INTO account_types(id, name) VALUES  (0, 'Internal transaction');
 INSERT INTO account_types(id, name) VALUES (1, '500+');
 INSERT INTO account_types(id, name) VALUES (2, 'Regular Account');
 INSERT INTO account_types(id, name) VALUES (3, 'Platinum Account');
-INSERT INTO account_types(id, name) VALUES  (0, 'Internal transaction');
+INSERT INTO account_types(id, name) VALUES  (4, 'CREDIT');
+INSERT INTO account_types(id, name) VALUES  (5, 'DEPOSIT');
