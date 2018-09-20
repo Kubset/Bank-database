f1d377e2fff5e99c2e9536941cd553fc94346b54
Kubset <kuba1228334@gmail.com>
2018-09-20 12:52:41 +0200

    update scripts

--- run_scripts.sql
+++ run_scripts.sql
@@ -1,5 +1,7 @@
 BEGIN;
+\i /home/kuba/codecool/advanced/TW_2/Bank-database/custom_settings.sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/functions/getbalance@(integer).sql
+\i /home/kuba/codecool/advanced/TW_2/Bank-database/functions/create_account@().sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/functions/update_balances@().sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/functions/check_balance_before_transaction@().sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/functions/sum_credits@(integer).sql
@@ -11,11 +13,13 @@ BEGIN;
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/fill_database/fill_user_data.sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/fill_database/fill_accounts.sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/functions/fill_available_founds@().sql
-\i /home/kuba/codecool/advanced/TW_2/Bank-database/fill_database/fill_user_accounts.sql
+\i /home/kuba/codecool/advanced/TW_2/Bank-database/fill_database/fill_user_accounts_without_credits_and_deposits.sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/fill_database/fill_deposits.sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/fill_database/fill_credits.sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/functions/update_available_founds_with_credits@().sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/fill_database/fill_cards.sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/functions/triggers.sql
 \i /home/kuba/codecool/advanced/TW_2/Bank-database/fill_database/fill_transactions.sql
+\i /home/kuba/codecool/advanced/TW_2/Bank-database/sequences.sql
+\i /home/kuba/codecool/advanced/TW_2/Bank-database/indexes.sql
 COMMIT;
